1b0e4894772350a2f3f38ca635d182bfe3fe23ce
Grazielle Gualter <grazi.gualter@gmail.com>
2025-08-29 17:24:57 -0300

    Cria Estilização

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,23 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import './App.css'
-import Home from './assets/pages/home/Home'
-import Navbar from './components/navbar/Navbar'
 import Footer from './components/footer/Footer'
-
+import Navbar from './components/navbar/Navbar'
+import Login from './assets/pages/login/Login'
+import Cadastro from './assets/pages/cadastro/Cadastro'
+import Home from './assets/pages/home/Home'
 
 
 function App() {
-  
   return (
     <>
-       <BrowserRouter>
+        <BrowserRouter>
           <Navbar />
           <div className="min-h-[80vh]">
             <Routes>
-              <Route path="/" element={<Home />} />
+              <Route path="/" element={<Login />} />
               <Route path="/home" element={<Home />} />
+              <Route path="/cadastro" element={<Cadastro />} />
+              <Route path="/login" element={<Login />} />
             </Routes>
           </div>
           <Footer />
