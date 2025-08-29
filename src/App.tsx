3983b8e9374d2c9db48f157abb458e7c8cbbd4b6
Grazielle Gualter <grazi.gualter@gmail.com>
2025-08-29 16:35:21 -0300

    Adiciona biblioteca tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,25 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import './App.css'
-import Home from './assets/pages/home/home'
+import Home from './assets/pages/home/Home'
+import Navbar from './components/navbar/Navbar'
+import Footer from './components/footer/Footer'
+
 
 
 function App() {
   
   return (
     <>
-      <Home/>
+       <BrowserRouter>
+          <Navbar />
+          <div className="min-h-[80vh]">
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/home" element={<Home />} />
+            </Routes>
+          </div>
+          <Footer />
+        </BrowserRouter>
     </>
   )
 }
