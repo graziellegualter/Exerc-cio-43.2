8a979204f9e78fe2ffe662ecf6938b2f425dd838
Grazielle Gualter <grazi.gualter@gmail.com>
2025-09-05 12:45:00 -0300

    Cria CRUD Postagem

--- src/App.tsx
+++ src/App.tsx
@@ -9,6 +9,10 @@ import { AuthProvider } from './contexts/AuthContext'
 import ListaTemas from './components/tema/listatemas/ListaTemas'
 import FormTema from './components/tema/formtema/FormTema'
 import DeletarTema from './components/tema/deletartema/DeletarTema'
+import ListaPostagens from './components/postagem/listapostagens/ListaPostagens'
+import FormPostagem from './components/postagem/formpostagem/FormPostagem'
+import DeletarPostagem from './components/postagem/deletarpostagem/DeletarPostagem'
+import Perfil from './pages/perfil/Perfil'
 
 
 function App() {
@@ -27,6 +31,11 @@ function App() {
               <Route path="/cadastrartema" element={<FormTema />} />
               <Route path="/editartema/:id" element={<FormTema />} />
               <Route path="/deletartema/:id" element={<DeletarTema />} />
+              <Route path="/postagens" element={<ListaPostagens />} />
+              <Route path="/cadastrarpostagem" element={<FormPostagem />} />
+              <Route path="/editarpostagem/:id" element={<FormPostagem />} />
+              <Route path="/deletarpostagem/:id" element={<DeletarPostagem />} />
+              <Route path="/perfil" element={<Perfil />} />
             </Routes>
           </div>
           <Footer />
